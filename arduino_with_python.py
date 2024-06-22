6680e404d721f46fe400c942c6f332fcbb79ceda
Deepanshu Yadav <deeepanshu3107@gmail.com>
2024-06-22 20:15:43 +0530

    Arduino Measuring road Cylinder

--- arduino_with_python.py
+++ arduino_with_python.py
@@ -3,3 +3,10 @@ import serial
 from visual import *
 
 arduino_Serial_Data=serial.Serial("com11","9600")
+measuringRod=cylinder(length=6,color=color.yellow,raduis=.5,pos=(-3,0,0))
+while(1==1):
+    rate(20)
+    if (arduino_Serial_Data.in_waiting>0):
+        mydata=arduino_Serial_Data.readlines()
+        distance=float(arduino_Serial_Data)
+
